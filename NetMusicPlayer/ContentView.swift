797bcdeb744db2402871f0ba5ecdde577622a772
takesi <nanasetakesi@yahoo.co.jp>
2021-06-28 22:26:37 +0800

    Initial Commit

---
+++ NetMusicPlayer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NetMusicPlayer
+//
+//  Created by TAKESI NANASE on 2021/6/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NetMusicPlayer/NetMusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetMusicPlayerApp.swift
+//  NetMusicPlayer
+//
+//  Created by TAKESI NANASE on 2021/6/28.
+//
+
+import SwiftUI
+
+@main
+struct NetMusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
